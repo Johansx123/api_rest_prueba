b8a808b528e57034f504791e7e8d7e834633c173
Johan Gomez <johansx123@gmail.com>
2023-07-07 20:35:16 -0500

    a

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,6 @@
-import {pool} from '../db.js' // importamos la conexion db
+//import {pool} from '../db.js' // importamos la conexion db
 
 export const ping = async (req, res) => { // funcion async para esperar el resultado de la db
-    const result = await pool.query('SELECT 1 + 1 AS result');
-    res.json(result); // respuesta de la db
+   // const result = await pool.query('SELECT 1 + 1 AS result');
+    res.json("SI SIRVE CTM"); // respuesta de la db
 }
