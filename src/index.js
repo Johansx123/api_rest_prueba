842280d2ea3e59a9b77a7d8b319f9b9e297f9786
Johan Gomez <johansx123@gmail.com>
2023-07-07 19:55:23 -0500

    a

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import app from './app.js'
 import {PORT} from './config.js'
 
 app.listen(PORT); // entra en modo de escucha en el puerto 3000
-console.log('Server running on port', PORT)
+res.json({ message: "ready in port"}, PORT)
