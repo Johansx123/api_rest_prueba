32a3b66b99c67bb49be4e23b67ebf3891d723b2c
Johan Gomez <johansx123@gmail.com>
2023-07-07 20:32:37 -0500

    a

---
+++ index.js
@@ -0,0 +1,4 @@
+import app from './src/app.js'
+import {PORT} from './src/config.js'
+
+app.listen(PORT); // entra en modo de escucha en el puerto 3000
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import app from './app.js'
-import {PORT} from './config.js'
-
-app.listen(PORT); // entra en modo de escucha en el puerto 3000
