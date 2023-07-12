1fe08568fc4e644670f761c59b75ef54a88a39de
Johan Gomez <johansx123@gmail.com>
2023-07-11 22:50:25 -0500

    create_comments

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -11,3 +11,21 @@ export const getComments = async (req, res) => {
     }
    
 };
+
+export const CreateComment = async (req, res) => {
+    try {
+        const {comment_customer, qualification} = req.body;
+        const [rows] = await pool.query('INSERT INTO u175710332_handymend.comments(comment_customer, qualification) VALUES (?, ?)',[comment_customer , qualification]);
+        res.send(
+            {
+            id: rows.insertId,
+            comment_customer: comment_customer,
+            qualification: qualification
+            });
+    } catch (error) {
+        return res.status(500).json ({
+            message: 'something goes weong'
+        })  
+    }
+   
+};
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
-import { getComments } from '../controllers/comments.controller.js';
+import { getComments, CreateComment } from '../controllers/comments.controller.js';
 
 const router = Router();
 router.get('/api/comments', getComments );
+router.post('/api/comments', CreateComment );
 export default router;
