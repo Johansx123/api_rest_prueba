8ea9cd45b85d602f0e615c37495097f77d1cec7f
Johan Gomez <johansx123@gmail.com>
2023-07-11 22:40:12 -0500

    a

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import  express  from 'express'; // importamos express
 import indexRoutes from './routes//index.routes.js';
 import employessRoutes from './routes/employes.routes.js' // importo las routes de la api
 import services from './routes/services.routes.js'
+import comments from './routes/comment.routes.js'
 
 const app = express(); // renombramos express por app
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(services);
 app.use(indexRoutes);
 app.use(employessRoutes);
+app.use(comments)
 
 app.use((req,res,next)=> {
     res.status(404).json({
