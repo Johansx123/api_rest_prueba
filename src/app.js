d696ca1b6c2a778eb3f3448ed2921ca6b8265df4
Johan Gomez <johansx123@gmail.com>
2023-07-15 11:25:42 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -5,8 +5,13 @@ import services from './routes/services.routes.js'
 import comments from './routes/comment.routes.js'
 import users from './routes/users.routes.js'
 import requestservices from './routes/requestservice.routes.js'
+import cors from 'cors';
+
+
+
 const app = express(); // renombramos express por app
 app.use(express.json());
+app.use(cors());
 
 app.use(services,indexRoutes,employessRoutes,comments,users,requestservices);
 
