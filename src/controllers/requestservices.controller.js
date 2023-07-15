ad3f4b6fbc8dcd1e16f86d5be430dc17535929ab
Johan Gomez <johansx123@gmail.com>
2023-07-15 14:36:12 -0500

    a

--- src/controllers/requestservices.controller.js
+++ src/controllers/requestservices.controller.js
@@ -2,15 +2,15 @@ import { pool } from "../db.js";
 
 export const CreateRequestService = async (req, res) => {
     try {
-        const {customer_id, service_id,customer_name} = req.body;
-        await pool.query('INSERT INTO u175710332_handymend.customers(customer_id,customer_name) VALUES (?,?)',[customer_id,customer_name ]);
-        const [rows] = await pool.query('INSERT INTO u175710332_handymend.request_services(customer_id, service_id) VALUES (?, ?)',[customer_id , service_id]);
+        const {phone, service,name} = req.body;
+        await pool.query('INSERT INTO u175710332_handymend.customers(customer_id,customer_name) VALUES (?,?)',[phone,name ]);
+        const [rows] = await pool.query('INSERT INTO u175710332_handymend.request_services(customer_id, service_id) VALUES (?, ?)',[phone , service]);
         res.send(
             {
             id: rows.insertId,
-            customer_name: customer_name,
-            customer_id: customer_id,
-            service_id: service_id
+            name: name,
+            phone: phone,
+            service: service
             });
     } catch (error) {
         return res.status(500).json ({
